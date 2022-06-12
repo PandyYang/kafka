d9dc2fa82a0790059300c7bc4a5848a4d97f0e3b
PandyYang <yangpandy@gmail.com>
2022-06-12 22:17:58 +0800

    自定义分区器

---
+++ src/main/java/com/pandy/kafka/producer/MyPartitioner.java
@@ -0,0 +1,36 @@
+package com.pandy.kafka.producer;
+
+import org.apache.kafka.clients.producer.Partitioner;
+import org.apache.kafka.common.Cluster;
+
+import java.util.Map;
+
+/**
+ * @author: Pandy
+ * @create: 2022/6/12
+ **/
+public class MyPartitioner implements Partitioner {
+    @Override
+    public int partition(String s, Object o, byte[] bytes, Object o1, byte[] bytes1, Cluster cluster) {
+        // 获取数据
+        String msgValues = o1.toString();
+
+        int partition;
+        if (msgValues.contains("pandy")) {
+            partition = 0;
+        } else {
+            partition = 1;
+        }
+        return partition;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void configure(Map<String, ?> map) {
+
+    }
+}
